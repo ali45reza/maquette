c57c083f98d5a5f90acc6863cacad529f6cef5d5
ali45reza <139990270+ali45reza@users.noreply.github.com>
2023-08-22 16:43:50 +0200

    Add files via upload
    freemind avec les balise CSS/HTML en plus de Flexbox Et fonctionalité de Algorithme

---
+++ algorithme.mm
@@ -0,0 +1,170 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cc0000" COLOR="#ffffff" CREATED="1692684705691" ID="ID_574422512" MODIFIED="1692687084758" STYLE="bubble" TEXT="ALGORITHMIQUE">
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692687999873" HGAP="-67" ID="ID_630245453" MODIFIED="1692688933591" POSITION="right" TEXT="Langage et r&#xe8;gles s&quot;&#xe9;criture" VSHIFT="-45">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1692688052865" ID="ID_1941421374" MODIFIED="1692688289550" TEXT="Ecriture en LDA">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1692688072209" ID="ID_106442565" MODIFIED="1692688314992" TEXT="Langage de  descrition algorithmique" VSHIFT="-12">
+<node CREATED="1692688559289" ID="ID_1264221994" MODIFIED="1692688568746" TEXT="Mots cl&#xe9;s et structure"/>
+</node>
+</node>
+<node CREATED="1692688120229" HGAP="27" ID="ID_214087522" MODIFIED="1692688577288" TEXT="Algorigramme" VSHIFT="33">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1692688141865" ID="ID_1694772501" MODIFIED="1692688203231" TEXT="Repr&#xe9;sentation graphique ">
+<node CREATED="1692688208385" ID="ID_428876090" MODIFIED="1692688237862" TEXT="Symboles de traitement"/>
+<node CREATED="1692688209940" ID="ID_880865872" MODIFIED="1692688248409" TEXT="Symboles de test"/>
+<node CREATED="1692688210700" ID="ID_948126321" MODIFIED="1692688258734" TEXT="Symboles auxili&#xe8;res"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692687659929" HGAP="-28" ID="ID_46239086" MODIFIED="1692690041855" POSITION="right" TEXT="Squelette" VSHIFT="-63">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1692687672465" HGAP="60" ID="ID_1347562921" MODIFIED="1692689833847" TEXT="La pr&#xe9;paration de traitement" VSHIFT="-11">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1692688336953" HGAP="32" ID="ID_436049606" MODIFIED="1692689213621" TEXT="Rep&#xe9;rer les donn&#xe9;es" VSHIFT="-29">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1692688375456" HGAP="26" ID="ID_1368343934" MODIFIED="1692689111499" TEXT="Num&#xe9;rique" VSHIFT="-17">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1692688458384" ID="ID_1694383348" MODIFIED="1692688830039" TEXT="Chiffres, Nombres" VSHIFT="2"/>
+</node>
+<node CREATED="1692688380252" HGAP="27" ID="ID_210499899" MODIFIED="1692689074413" TEXT="Textuelle" VSHIFT="2">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1692688473592" HGAP="31" ID="ID_931453590" MODIFIED="1692688827135" TEXT="Caract&#xe8;res,chaine de caract&#xe8;res" VSHIFT="2"/>
+</node>
+<node CREATED="1692688702684" ID="ID_1103759413" MODIFIED="1692689071463" TEXT="Bool&#xe9;enne" VSHIFT="19">
+<icon BUILTIN="full-3"/>
+<node CREATED="1692688735224" FOLDED="true" HGAP="28" ID="ID_916381355" MODIFIED="1692688956160" TEXT=" Type logique &#xe0; 2 valeurs possibles" VSHIFT="2">
+<node CREATED="1692688776160" HGAP="17" ID="ID_1512408603" MODIFIED="1692688871327" TEXT="(0 ou 1) , (oui ou non)" VSHIFT="3"/>
+</node>
+</node>
+<node CREATED="1692689125531" HGAP="21" ID="ID_805505615" MODIFIED="1692689155767" TEXT="Graphique" VSHIFT="22">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1692688368188" HGAP="45" ID="ID_1937365911" MODIFIED="1692689284903" TEXT="Nommer l&apos;Algorithme" VSHIFT="-77">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1692689245643" HGAP="30" ID="ID_895895616" MODIFIED="1692689296620" TEXT="L&apos;Algorithme se compose de :" VSHIFT="5">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1692689301561" ID="ID_1698373210" MODIFIED="1692689344607" TEXT="Contantes">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1692689310467" ID="ID_76871755" MODIFIED="1692689347854" TEXT="Variable">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1692689335828" ID="ID_1466825929" MODIFIED="1692689351006" TEXT="Structures">
+<icon BUILTIN="full-3"/>
+<node CREATED="1692689445976" HGAP="28" ID="ID_1863038367" MODIFIED="1692690087486" TEXT="Liste exhaustive utilis&#xe9; dans le corps de l&apos;algorithme" VSHIFT="1">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692687673877" HGAP="63" ID="ID_1251526798" MODIFIED="1692691049356" TEXT="Les traitement des donn&#xe9;es" VSHIFT="34">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689654761" HGAP="19" ID="ID_1079542344" MODIFIED="1692690659334" TEXT="Entr&#xe9;es" VSHIFT="18">
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689665593" HGAP="49" ID="ID_1640159707" MODIFIED="1692690376131" TEXT="Clavier" VSHIFT="-13">
+<icon BUILTIN="full-1"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689669915" HGAP="47" ID="ID_525179549" MODIFIED="1692690376131" TEXT="Souris">
+<icon BUILTIN="full-2"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689676755" HGAP="44" ID="ID_444235561" MODIFIED="1692690376131" TEXT="Micro" VSHIFT="13">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692690345360" HGAP="51" ID="ID_492940045" MODIFIED="1692691049356" TEXT="Corps de l&apos;algorithme" VSHIFT="2">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692690398376" HGAP="29" ID="ID_203722201" MODIFIED="1692690605158" TEXT="Sp&#xe9;cification des &#xe9;tapes des instructions" VSHIFT="-17"/>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692690447584" HGAP="33" ID="ID_1118508641" MODIFIED="1692690787930" TEXT="Les Types" VSHIFT="29">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692690461632" HGAP="16" ID="ID_602513873" MODIFIED="1692690608574" TEXT="Nature des instructions" VSHIFT="-28"/>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692690513622" HGAP="34" ID="ID_1709529581" MODIFIED="1692690774162" TEXT="Algorithme S&#xe9;quentielle (mono coeur)" VSHIFT="1">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1692690549618" HGAP="31" ID="ID_494317838" MODIFIED="1692690700300" TEXT="Algorithme Parall&#xe8;le (multi coeur)" VSHIFT="26">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1692690708946" HGAP="35" ID="ID_1053088619" MODIFIED="1692690808070" TEXT=" Algorithme r&#xe9;parti ou distribu&#xe9; (r&#xe9;seau de processeur)" VSHIFT="36">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1692687674678" HGAP="62" ID="ID_854433499" MODIFIED="1692691076658" TEXT="La sortie des r&#xe9;sultats" VSHIFT="-3">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-3"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689694561" HGAP="64" ID="ID_1662338828" MODIFIED="1692690997022" TEXT="Sortie" VSHIFT="3">
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689945207" HGAP="47" ID="ID_460011803" MODIFIED="1692691002406" TEXT="Imprimante" VSHIFT="-48">
+<icon BUILTIN="full-1"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692689946810" HGAP="62" ID="ID_1168932714" MODIFIED="1692691005430" TEXT="haut parleurs" VSHIFT="-35">
+<icon BUILTIN="full-2"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692690000458" HGAP="119" ID="ID_879720995" MODIFIED="1692691021838" TEXT="Ecran" VSHIFT="-22">
+<icon BUILTIN="full-3"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692690890130" HGAP="104" ID="ID_666109938" MODIFIED="1692691017494" TEXT="Fichier" VSHIFT="5">
+<icon BUILTIN="full-4"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692690951625" HGAP="81" ID="ID_99911318" MODIFIED="1692691024070" TEXT="M&#xe9;moire" VSHIFT="40">
+<icon BUILTIN="full-5"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692690923521" HGAP="42" ID="ID_1100955616" MODIFIED="1692691026438" TEXT="Perdu" VSHIFT="41">
+<icon BUILTIN="full-6"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1692684716719" HGAP="74" ID="ID_952464824" MODIFIED="1692689842847" POSITION="right" TEXT="Composante essentielle activit&#xe9; math&#xe9;matique" VSHIFT="-56"/>
+<node BACKGROUND_COLOR="#00ff66" CREATED="1692687402226" HGAP="13" ID="ID_1618460305" MODIFIED="1692689392911" POSITION="left" TEXT="Un Algorithme d&#xe9;crit un processus de r&#xe9;solution&#xa;d&apos;un probl&#xe8;me d&#xe9;fini et r&#xe9;diger dans un langage formalis&#xe9;&#xa; et produit un r&#xe9;sultat dans un temps  fini." VSHIFT="-78">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1692687551729" HGAP="42" ID="ID_936191056" MODIFIED="1692687835065" TEXT="Ensembre d&apos;instructions" VSHIFT="-15">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1692687615828" HGAP="61" ID="ID_453888648" MODIFIED="1692687836816" TEXT="Un ordre">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1692687616804" HGAP="42" ID="ID_453433704" MODIFIED="1692687829241" TEXT="Un d&#xe9;but et une fin" VSHIFT="18">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1692684758788" HGAP="38" ID="ID_657114407" MODIFIED="1692689388615" POSITION="left" TEXT="Quotidien-Base des ordinateur Automates" VSHIFT="-181">
+<node CREATED="1692684956192" ID="ID_170840643" MODIFIED="1692684969384" TEXT="Automates"/>
+<node CREATED="1692684975069" ID="ID_896114200" MODIFIED="1692684980848" TEXT="Ordinateurs"/>
+<node CREATED="1692684981660" ID="ID_1221323898" MODIFIED="1692684993872" TEXT="Calculatrice"/>
+</node>
+<node CREATED="1692685080256" HGAP="95" ID="ID_202238973" MODIFIED="1692689845303" POSITION="right" TEXT="Antiquit&#xe9;" VSHIFT="-11">
+<node CREATED="1692685097162" ID="ID_909903347" MODIFIED="1692686059033" TEXT="Calcul la somme de 2 nombres entiers"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685137032" HGAP="96" ID="ID_147555185" MODIFIED="1692687363818" POSITION="left" TEXT="Comprendre et examiner un Algoriyhme" VSHIFT="-38"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685174880" HGAP="89" ID="ID_129130122" MODIFIED="1692689850039" POSITION="right" TEXT="Modifier un Algorithme pour obtenir un r&#xe9;sultat pr&#xe9;cis" VSHIFT="29"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685267719" ID="ID_1629534776" MODIFIED="1692685398460" POSITION="left" TEXT="Cr&#xe9;er une solution Algorithmique"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685352840" HGAP="26" ID="ID_1363616608" MODIFIED="1692685780487" POSITION="left" TEXT="Valider la solution ALGORITHMIQUE" VSHIFT="54"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685471385" HGAP="-6" ID="ID_713766008" MODIFIED="1692685782566" POSITION="left" TEXT="Savoir comment &#xe9;crire un langage courant tout en respectant un code" VSHIFT="50"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685591664" HGAP="-11" ID="ID_938698976" MODIFIED="1692685786511" POSITION="left" TEXT="Identifier les boucles, les testes les op&#xe9;rations l&apos;&#xe9;criture" VSHIFT="86"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685270376" HGAP="67" ID="ID_1319084443" MODIFIED="1692685663623" POSITION="right" TEXT="Analyser une instruction et v&#xe9;rifier les donn&#xe9;es" VSHIFT="23"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685658648" HGAP="55" ID="ID_533667721" MODIFIED="1692689866495" POSITION="right" TEXT="Valider les solutions algorithmiques " VSHIFT="40"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685702679" HGAP="41" ID="ID_1365702297" MODIFIED="1692689862895" POSITION="right" TEXT="Adapter l&apos;algorithme aux contraintes du langage de programmation" VSHIFT="40">
+<font BOLD="true" NAME="SansSerif" SIZE="11"/>
+</node>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1692685742128" HGAP="1" ID="ID_353080002" MODIFIED="1692689859959" POSITION="right" TEXT="Valider les programmes simples" VSHIFT="34">
+<font NAME="SansSerif" SIZE="13"/>
+</node>
+<node BACKGROUND_COLOR="#ff0000" CREATED="1692687158834" HGAP="-68" ID="ID_1451646077" MODIFIED="1692689854327" POSITION="right" TEXT="Un Algorithme est une suite finie d&apos;instructions &#xe9;l&#xe9;mentaires&#xa; (des r&#xe8;gles qui s&apos;appique dans un ordre d&#xe9;termin&#xe9;&#xa; &#xe0; un nombre fini de donn&#xe9;es pour fournir un r&#xe9;sultat." VSHIFT="30">
+<font NAME="SansSerif" SIZE="15"/>
+</node>
+</node>
+</map>
